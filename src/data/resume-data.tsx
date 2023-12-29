c823455b93450d15c4377bbd7e43bbd514b72ebf
BO <130245932+ElBryano@users.noreply.github.com>
2023-12-29 00:36:59 -0600

    updated

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -48,36 +48,36 @@ export const RESUME_DATA = {
       link: "https://www.linkedin.com/company/drive-x/",
       badges: ["Data Analytics"],
       title: "Teaching Assistant - University of The Bahamas",
-      logo: "https://media.licdn.com/dms/image/C4E0BAQH80OD6UBrp7w/company-logo_200_200/0/1674943725742/drive_x_logo?e=1711584000&v=beta&t=WfpZYY3Ewh5FbtaseDKhIERBcnHSPj0Xlhxrq28Cbww", // Add logo if available
+      logo: "", // Add logo if available
       start: "Dec 2023",
       end: "Present",
       description: (
         <>
-          <li>
+          <>
             Supported kickoff of new data analytics career pilot program through University of The Bahamas, helping twenty-eight working professionals reskill into data-focused roles.
-          </li>
-          <li>
+          </>
+          <>
             Assisted with SQL (PostgreSQL, MySQL), Tableau, Excel, and Python skill-building modules to further strengthen students' technical capabilities with essential data tools as regional technology sector expands.
-          </li>
+          </>
         </>
-      ),
+      )
     },
     {
       company: "General Assembly",
       link: "https://generalassemb.ly/",
       badges: ["Data Science / Machine Learning"],
       title: "Teaching Assistant - Data Science Immersive Fellowship",
-      logo: "https://media.licdn.com/dms/image/C4E0BAQEwAxD22k-HBw/company-logo_200_200/0/1630655071296/generalassembly_logo?e=1711584000&v=beta&t=zCubQoXEcwrHNUinalIYg5RFprAoBxsjFfptmBQ4rYo", // Add logo if available
+      logo: "", // Add logo if available
       start: "Nov 2023",
       end: "Present",
       description: (
         <>
-          <li>
+          <>
             Partnered with two other instructors to deliver a 12-week comprehensive Data Science Immersive training program, covering the full Data Science toolkit, including Data Visualizations, Python (Seabon, Matplotlib, Pandas), R, SQL (BigQuery, SQLite), Tableau, and Machine Learning.
-          </li>
-          <li>
+          </>
+          <>
             Provided personalized technical guidance to ten students of varying skill levels, overseeing grading rubrics and assessments across key program milestones while assisting with hands-on projects.
-          </li>
+          </>
         </>
       ),
     },
@@ -86,17 +86,17 @@ export const RESUME_DATA = {
       link: "https://www.loreal.com/en/",
       badges: ["Data Literacy"],
       title: "Instructional Assistant - Data Literacy Program",
-      logo: "https://media.licdn.com/dms/image/D4E0BAQFEB_g-FWxSrQ/company-logo_200_200/0/1688397792617/loral_logo?e=1711584000&v=beta&t=y1fFQ9xMERft3GwkkH4ZJyvacfdQzsh12yziOF2LXsQ", // Add logo if available
+      logo: "", // Add logo if available
       start: "Nov 2023",
       end: "Present",
       description: (
         <>
-          <li>
+          <>
             Assisted the lead instructor for L'Oréal's Data Literacy Program (200+ nontechnical participants), covering data democratization best practices, full data lifecycle phases like ETL and data modeling, and introducing data visualization tools like Tableau and PowerBI for transforming processed data into business insights.
-          </li>
-          <li>
+          </>
+          <>
             Collaborated with L'Oréal's team to ensure alignment of expectation for the program. Responsible for polls/trivia questions to keep participants engaged and answer questions, enabling the lead instructor to present smoothly.
-          </li>
+          </>
         </>
       ),
     },
@@ -105,17 +105,17 @@ export const RESUME_DATA = {
       link: "https://www.facebook.com/FocussInc/",
       badges: ["Python Programming"],
       title: "Data Scientist",
-      logo: "https://scontent-msp1-1.xx.fbcdn.net/v/t39.30808-6/279302060_182805384077232_8387082812529376351_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=Q8eur6c6MZQAX_xcNa6&_nc_ht=scontent-msp1-1.xx&oh=00_AfCRmAcAWOcYbGTMaAshJLsXTI50JG7qBl2OjZCg0d6Ddg&oe=6592141F",
+      logo: "",
       start: "Oct 2023",
       end: "Present",
       description: (
         <>
-          <li>
+          <>
             Leading the creation of a comprehensive Python coding curriculum for high school district within the FOCUSS group.
-          </li>
-          <li>
+          </>
+          <>
             Foster student learning and engagement through hands-on coding exercises and projects in Jupyter notebooks/integrated development environments (IDEs) like VSCode, while leveraging bilingual fluency in Spanish and English to build interpersonal connections with broader range of students across language barriers.
-          </li>
+          </>
         </>
       ),
     },
