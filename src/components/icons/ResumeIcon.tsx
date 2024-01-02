eebe18a702307e2177ebbfb79844941bfdc49e18
BO <130245932+ElBryano@users.noreply.github.com>
2024-01-01 22:00:07 -0600

    retry

--- src/components/icons/ResumeIcon.tsx
+++ src/components/icons/ResumeIcon.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ResumeIcon = (props) => (
+const ResumeIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path 
-      fill="currentColor"
-      d="M20 6h-3V4c0-1.11-.89-2-2-2H9c-1.11 0-2 .89-2 2v2H4c-1.11 0-2 .89-2 2v11c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zM9 4h6v2H9V4zm11 15H4v-2h16v2zm0-5H4V8h3v2h2V8h6v2h2V8h3v6z"/>
+    <path
+      fill="currentColor"  
+      d="M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zM12 5c3.87 0 7 3.13 7 7 0 1.37-.35 2.67-.95 3.8l1.01 1.01C20.35 15.73 21 14.48 21 13c0-3.87-3.13-7-7-7z"/>
   </svg>
 );
 
