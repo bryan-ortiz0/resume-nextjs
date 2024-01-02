8c72a19544879afd51e6713666f2bf761c1dc50b
BO <130245932+ElBryano@users.noreply.github.com>
2024-01-01 21:26:57 -0600

    going back to the other one that i liked

--- src/components/icons/ResumeIcon.tsx
+++ src/components/icons/ResumeIcon.tsx
@@ -5,11 +5,7 @@ const ResumeIcon = (props: React.SVGProps<SVGSVGElement>) => {
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         fill="currentColor"
-        d="M12 2a10 10 0 0 1 10 10v6h2v-6a8 8 0 1 0-16 0v6h2v-6a10 10 0 0 1 10-10zm0 14a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"
-      />
-      <path
-        fill="currentColor"
-        d="M9 2a7 7 0 0 0-7 7v9h14v-9a7 7 0 0 0-7-7zm12 14h-2.17a.5.5 0 0 1-.64-.49l-2.24-2.24a.5.5 0 0 1 .26-.75l3-3a.5.5 0 0 1 .75.26l2.24 2.24a.5.5 0 0 1-.49.64z"
+        d="M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18zm0-2a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm0-9a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
       />
     </svg>
   );
