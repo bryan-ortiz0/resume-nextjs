f4bfdfde2ea4e457cfb1119d40b4c2fad742d5da
BO <130245932+ElBryano@users.noreply.github.com>
2024-01-26 15:00:47 -0600

    another attempt

--- src/app/.ipynb_checkpoints/page-checkpoint.tsx
+++ src/app/.ipynb_checkpoints/page-checkpoint.tsx
@@ -1,15 +1,15 @@
-import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
-import { Card, CardHeader, CardContent } from "../components/ui/card";
-import { Badge } from "../components/ui/badge";
-import { CommandMenu } from "../components/command-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { CommandMenu } from "@/components/command-menu";
 import { Metadata } from "next";
-import { Section } from "../components/ui/section";
+import { Section } from "@/components/ui/section";
 import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
-import { Button } from "../components/ui/button";
-import { RESUME_DATA } from "../data/resume-data";
-import { ProjectCard } from "../components/project-card";
+import { Button } from "@/components/ui/button";
+import { RESUME_DATA } from "@/data/resume-data";
+import { ProjectCard } from "@/components/project-card";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
   description: RESUME_DATA.summary,
 };
@@ -17,7 +17,7 @@ export const metadata = {
 export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-      <section className="mx-auto w-full max-w-3xl space-y-8 bg-white print:space-y-6">
+      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
             <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
@@ -30,28 +30,45 @@ export default function Page() {
                 href={RESUME_DATA.locationLink}
                 target="_blank"
               >
-                <GlobeIcon className="h-3 w-3" />
+                <GlobeIcon className="size-3" />
                 {RESUME_DATA.location}
               </a>
             </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
               {RESUME_DATA.contact.email ? (
-                <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                  <Button className="h-8 w-8" variant="outline" size="icon">
-                    <MailIcon className="h-4 w-4" />
-                  </Button>
-                </a>
+                <Button
+                  className="size-8"
+                  variant="outline"
+                  size="icon"
+                  asChild
+                >
+                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                    <MailIcon className="size-4" />
+                  </a>
+                </Button>
               ) : null}
-          
+              {RESUME_DATA.contact.tel ? (
+                <Button
+                  className="size-8"
+                  variant="outline"
+                  size="icon"
+                  asChild
+                >
+                  <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                    <PhoneIcon className="size-4" />
+                  </a>
+                </Button>
+              ) : null}
               {RESUME_DATA.contact.social.map((social) => (
                 <Button
                   key={social.name}
-                  className="h-8 w-8"
+                  className="size-8"
                   variant="outline"
                   size="icon"
+                  asChild
                 >
                   <a href={social.url}>
-                    <social.icon className="h-4 w-4" />
+                    <social.icon className="size-4" />
                   </a>
                 </Button>
               ))}
@@ -62,10 +79,15 @@ export default function Page() {
                   <span className="underline">{RESUME_DATA.contact.email}</span>
                 </a>
               ) : null}
+              {RESUME_DATA.contact.tel ? (
+                <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                  <span className="underline">{RESUME_DATA.contact.tel}</span>
+                </a>
+              ) : null}
             </div>
           </div>
 
-          <Avatar className="h-28 w-28">
+          <Avatar className="size-28">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
@@ -76,7 +98,7 @@ export default function Page() {
             {RESUME_DATA.summary}
           </p>
         </Section>
-          
+        
         <Section>
           <h2 className="text-xl font-bold">Skills</h2>
           <div className="flex flex-wrap gap-1">
@@ -84,8 +106,7 @@ export default function Page() {
               return <Badge key={skill}>{skill}</Badge>;
             })}
           </div>
-        </Section>
-      </section>
+        </Section>  
           
         <Section>
           <h2 className="text-xl font-bold">Work Experience</h2>
@@ -127,10 +148,10 @@ export default function Page() {
             );
           })}
         </Section>
-
+        
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
-          <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-1 print:gap-2 md:grid-cols-1 lg:grid-cols-1">
+          <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-1 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
               return (
                 <ProjectCard
@@ -143,7 +164,8 @@ export default function Page() {
               );
             })}
           </div>
-        </Section>
+        </Section>  
+          
         <Section>
           <h2 className="text-xl font-bold">Education</h2>
           {RESUME_DATA.education.map((education) => {
@@ -164,9 +186,9 @@ export default function Page() {
             );
           })}
         </Section>
-        
+      </section>
 
-       <CommandMenu
+      <CommandMenu
         links={[
           ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
             url: socialMediaLink.url,
