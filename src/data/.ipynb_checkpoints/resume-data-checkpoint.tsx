982ce00e6e8d2aec2af4edf3b2624397422143e4
BO <130245932+ElBryano@users.noreply.github.com>
2024-01-30 19:29:30 -0500

    updated

--- src/data/.ipynb_checkpoints/resume-data-checkpoint.tsx
+++ src/data/.ipynb_checkpoints/resume-data-checkpoint.tsx
@@ -19,7 +19,7 @@ export const RESUME_DATA = {
     social: [
       {
         name: "Resume",
-        url: "https://drive.google.com/file/d/1Cr-v2JSDGeQ3nSpXBpc_pe8QoqKG8Cm2/view?usp=drive_link",
+        url: "https://drive.google.com/file/d/1Qpn1cDk-Bb9DAjfylm7NW8Pyyd0kZsdg/view?usp=sharing",
         icon: ResumeIcon,
       },
       {
