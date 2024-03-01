de137669c7dca095b8829380c8748cdeb8f327fc
Bryan Ortiz <ortizbryan54321@gmail.com>
2024-03-01 10:38:29 -0600

    reformatted and added certs

--- src/app/.ipynb_checkpoints/page-checkpoint.tsx
+++ src/app/.ipynb_checkpoints/page-checkpoint.tsx
@@ -90,15 +90,6 @@ export default function Page() {
               );
             })}
           </div>
-        </Section>
-        
-        <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
-          <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
-              return <Badge key={skill}>{skill}</Badge>;
-            })}
-          </div>
         </Section>  
           
         <Section>
@@ -161,6 +152,15 @@ export default function Page() {
               </Card>
             );
           })}
+        </Section>
+          
+        <Section>
+          <h2 className="text-xl font-bold">Skills</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.skills.map((skill) => {
+              return <Badge key={skill}>{skill}</Badge>;
+            })}
+          </div>
         </Section>
         
         <Section className="print-force-new-page scroll-mb-16">
